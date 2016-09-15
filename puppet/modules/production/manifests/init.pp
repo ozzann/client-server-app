bb56f0d63bd7c5f04dec6e58608032543d277372
Anna Slastnikova <anna.slastnikova@gmail.com>
2016-09-15 13:18:28 +0100

    fix puppet; rm target dir

--- puppet/modules/production/manifests/init.pp
+++ puppet/modules/production/manifests/init.pp
@@ -2,23 +2,37 @@ class production {
 
     require docker
 
-    exec {'rm -rf /home/vagrant/app/*':
+    exec {'rm -rf /home/vagrant/client-app/*':
         path => '/usr/bin:/usr/sbin:/bin',
     }
 
-    file { '/home/vagrant/app':
+    file { '/home/vagrant/client-app':
         ensure => 'directory',
-        source => 'puppet:///extra_files/app',
+        source => 'puppet:///extra_files/client-app',
         recurse => 'remote',
-        path => '/home/vagrant/app',
+        path => '/home/vagrant/client-app',
         owner => 'vagrant',
     }
-    
-    exec { 'deploy_app':
-       cwd     => "/home/vagrant/app",
-       logoutput => 'true',
-       command => "/bin/bash -c 'chmod +x deploy.sh; ./deploy.sh'",
-       require => File["/home/vagrant/app"],
-       timeout => 1000,
+
+    exec {'rm -rf /home/vagrant/server-app/*':
+        path => '/usr/bin:/usr/sbin:/bin',
+    }
+
+    file { '/home/vagrant/server-app':
+        ensure => 'directory',
+        source => 'puppet:///extra_files/server-app',
+        recurse => 'remote',
+        path => '/home/vagrant/server-app',
+        owner => 'vagrant',
+    }
+
+    file { '/home/vagrant/docker-compose.yml':
+        ensure => 'present',
+        source => 'puppet:///extra_files/docker-compose.yml',
+        owner => 'vagrant',
+    }
+
+    docker_compose { '/home/vagrant/docker-compose.yml':
+        ensure => present,
     }
 }
