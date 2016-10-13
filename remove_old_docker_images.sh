684a42d553dece1f0f9f032617ddc7865622fcb1
ozzann <anna.slastnikova@gmail.com>
2016-10-13 10:20:55 +0100

    delete old images properly

--- remove_old_docker_images.sh
+++ remove_old_docker_images.sh
@@ -1,8 +1,17 @@
+
 CONTAINERS=$(docker ps -q -a)
 if [ "$CONTAINERS" ]
 then
-	docker stop $CONTAINERS &> /dev/null
-	docker rm $CONTAINERS &> /dev/null
+        docker stop $CONTAINERS &> /dev/null
+        docker rm $CONTAINERS &> /dev/null
 fi
 
-docker rmi vagrant_server vagrant_client
+IMAGES_TO_DELETE=( "vagrant_client" "vagrant_server" )
+for IMAGE in "${IMAGES_TO_DELETE[@]}"
+do
+        DOCKER_IMAGE=$(docker images $IMAGE -q)
+        if [ "$DOCKER_IMAGE" ]
+        then
+                docker rmi $DOCKER_IMAGE
+        fi
+done
